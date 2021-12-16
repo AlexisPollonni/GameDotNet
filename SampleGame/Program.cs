d23f457b2c3b52eadcca751dd1db0133532c8b5a
AlexisPollonni <alexis.pollonni@gmail.com>
2021-12-16 15:27:41 +0100

    Added sample game project and initial code

---
+++ SampleGame/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Core;
+using Silk.NET.Windowing;
+
+Console.WriteLine("Hello, World!");
+
+var p = Window.Platforms;
+
+var app = new Application();
+
+return app.Run();
