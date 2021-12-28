9a2df84a2c5e00d9b2ec0c512d4330448d727b91
AlexisPollonni <alexis.pollonni@gmail.com>
2021-12-28 10:56:47 +0100

    fix typo in string constant

--- Core/Constants.cs
+++ Core/Constants.cs
@@ -8,7 +8,7 @@ public static class Constants
     public const string EngineName = "GamesDotNet";
 
 
-    public const string DebugUtilsExtensionName = "Vk_EXT_debug_utils";
+    public const string DebugUtilsExtensionName = "VK_EXT_debug_utils";
     public static Version32 EngineVersion => Vk.MakeVersion(0, 0, 1);
     public static string[] DefaultValidationLayers => new[] { "VK_LAYER_KHRONOS_validation" };
 }
