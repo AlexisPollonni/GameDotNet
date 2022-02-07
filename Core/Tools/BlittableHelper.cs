faca428c6563c7c1d3c2671fbdd528cfddab1557
AlexisPollonni <alexis.pollonni@gmail.com>
2022-02-07 15:51:38 +0100

    Added helpers to determine if object is blittable

---
+++ Core/Tools/BlittableHelper.cs
@@ -0,0 +1,49 @@
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace Core.Tools;
+
+public static class BlittableHelper
+{
+    //Avoids allocating handle for every types
+    private static GCHandle _gch;
+
+    static BlittableHelper()
+    {
+        _gch = GCHandle.Alloc(null, GCHandleType.Pinned);
+    }
+
+    public static bool IsBlittable<T>()
+    {
+        return IsBlittableCache<T>.Value;
+    }
+
+    private static bool IsBlittable(Type type)
+    {
+        if (type.IsArray)
+        {
+            var elem = type.GetElementType();
+            return (elem?.IsValueType ?? false) && IsBlittable(elem);
+        }
+
+        try
+        {
+            var instance = FormatterServices.GetUninitializedObject(type);
+
+            _gch.Target = instance; //TODO: Test this construct
+            //GCHandle.Alloc(instance, GCHandleType.Pinned).Free();
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    //From https://stackoverflow.com/a/31485271
+    private static class IsBlittableCache<T>
+    {
+        internal static readonly bool Value = IsBlittable(typeof(T));
+    }
+}
