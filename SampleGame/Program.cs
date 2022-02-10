7bc31190ada0111b13d7e8b7a62ebdff70dfa7a1
AlexisPollonni <alexis.pollonni@gmail.com>
2022-02-10 19:40:31 +0100

    Fix benchmark project and added NativeAot targets

--- SampleGame/Program.cs
+++ SampleGame/Program.cs
@@ -1,11 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Core;
-using Silk.NET.Windowing;
 
 Console.WriteLine("Hello, World!");
-
-var p = Window.Platforms;
 
 var app = new Application("SampleGame");
 
