d0e1dff8213f7cd913e50030049b6d2891118093
AlexisPollonni <alexis.pollonni@gmail.com>
2023-05-03 05:19:29 +0200

    Added disposable extensions for enumerables

--- GameDotNet.Core/Tools/Extensions/DisposableExtensions.cs
+++ GameDotNet.Core/Tools/Extensions/DisposableExtensions.cs
@@ -21,20 +21,34 @@ public static class DisposableMixins
     /// <param name="item">
     /// The disposable we are going to want to be disposed by the DisposableList.
     /// </param>
-    /// <param name="compositeDisposable">
+    /// <param name="disposable">
     /// The <see cref="DisposableList"/> to which <paramref name="item"/> will be added.
     /// </param>
-    /// <exception cref="ArgumentNullException"><paramref name="compositeDisposable"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="disposable"/> is <c>null</c>.</exception>
     /// <returns>
     /// The disposable.
     /// </returns>
-    public static T DisposeWith<T>(this T item, ICompositeDisposable compositeDisposable)
+    public static T DisposeWith<T>(this T item, ICompositeDisposable disposable)
         where T : IDisposable
     {
-        if (compositeDisposable is null)
-            throw new ArgumentNullException(nameof(compositeDisposable));
+        if (disposable is null)
+            throw new ArgumentNullException(nameof(disposable));
 
-        compositeDisposable.Add(item);
+        disposable.Add(item);
         return item;
+    }
+
+    public static IEnumerable<T>? DisposeAllWith<T>(this IEnumerable<T>? e, ICompositeDisposable disposable)
+        where T : IDisposable => e?.Select(item => item.DisposeWith(disposable));
+
+
+    public static void DisposeAll<T>(this IEnumerable<T>? e) where T : IDisposable
+    {
+        if (e is null) return;
+
+        foreach (var item in e)
+        {
+            item.Dispose();
+        }
     }
 }
