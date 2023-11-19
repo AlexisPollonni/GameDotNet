6a5ed86107f9fcf092f641161196cb568376fc48
AlexisPollonni <alexis.pollonni@gmail.com>
2023-11-19 23:33:51 +0100

    [Editor] switch to observable from async to add cancellation support

--- GameDotNet.Editor/ViewModels/WebGpuViewModel.cs
+++ GameDotNet.Editor/ViewModels/WebGpuViewModel.cs
@@ -1,7 +1,5 @@
 using System.Threading;
 using System.Threading.Tasks;
-using GameDotNet.Graphics;
-using GameDotNet.Graphics.Abstractions;
 using GameDotNet.Management.ECS;
 using Microsoft.Extensions.Logging;
 
@@ -11,33 +9,21 @@ public class WebGpuViewModel : ViewModelBase
 {
     private readonly ILogger<WebGpuViewModel> _logger;
     private readonly Universe _universe;
-    private readonly NativeViewManager _viewManager;
 
-    public WebGpuViewModel(ILogger<WebGpuViewModel> logger, Universe universe, NativeViewManager viewManager)
+    public WebGpuViewModel(ILogger<WebGpuViewModel> logger, Universe universe)
     {
         _logger = logger;
         _universe = universe;
-        _viewManager = viewManager;
     }
 
-    public void SetMainView(INativeView view)
-    {
-        _viewManager.MainView = view;
-    }
-
-    public async Task Initialize(CancellationToken token = default)
+    public async Task Run(CancellationToken token = default)
     {
         await _universe.Initialize(token);
 
-        Task.Run(() =>
+        await Task.Run(() =>
         {
-            while (true)
+            while (!token.IsCancellationRequested)
                 _universe.Update();
-        });
-    }
-
-    public void Update()
-    {
-        _universe.Update();
+        }, token);
     }
 }
--- GameDotNet.Editor/Views/WebGpuView.axaml.cs
+++ GameDotNet.Editor/Views/WebGpuView.axaml.cs
@@ -1,4 +1,7 @@
-using System.Threading.Tasks;
+using System;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using Avalonia.ReactiveUI;
 using GameDotNet.Editor.ViewModels;
 using ReactiveUI;
@@ -11,13 +14,19 @@ public partial class WebGpuView : ReactiveUserControl<WebGpuViewModel>
     {
         InitializeComponent();
 
-        this.WhenActivated(async d =>
+        this.WhenActivated(d =>
         {
-            await NativeControl.Initialize();
-            await (ViewModel?.Initialize() ?? Task.CompletedTask);
+            Observable.FromAsync(() => NativeControl.Initialize())
+                      .SelectMany(async (_, token) =>
+                      {
+                          if(ViewModel is not null)
+                            await ViewModel.Run(token);
+
+                          return Unit.Default;
+                      })
+                      .Subscribe()
+                      .DisposeWith(d);
         });
-        
-        
     }
 
 
