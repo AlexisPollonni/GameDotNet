3cab343a0d2efc968dbae492667360bc6bb1138b
AlexisPollonni <alexis.pollonni@gmail.com>
2023-12-20 16:03:06 +0100

    [Core] Added component properties to physics components to use in editor inspector

--- GameDotNet.Core/Physics/Components/Translation.cs
+++ GameDotNet.Core/Physics/Components/Translation.cs
@@ -4,6 +4,32 @@ namespace GameDotNet.Core.Physics.Components;
 
 public record struct Translation(Vector3 Value)
 {
+    private Vector3 _value = Value;
+
+    public Vector3 Value
+    {
+        readonly get => _value;
+        set => _value = value;
+    }
+
+    public float X
+    {
+        readonly get => Value.X;
+        set => _value.X = value;
+    }
+
+    public float Y
+    {
+        readonly get => Value.Y;
+        set => _value.Y = value;
+    }
+
+    public float Z
+    {
+        readonly get => Value.Z;
+        set => _value.Z = value;
+    }
+    
     public static implicit operator Vector3(in Translation t) => t.Value;
     public static implicit operator Translation(in Vector3 v) => new(v);
 }
--- GameDotNet.Management/ECS/Components/Tag.cs
+++ GameDotNet.Management/ECS/Components/Tag.cs
@@ -1,6 +1,3 @@
 namespace GameDotNet.Management.ECS.Components;
 
-public record struct Tag(string Name)
-{
-    public string Name = Name;
-}
+public record struct Tag(string Name);
