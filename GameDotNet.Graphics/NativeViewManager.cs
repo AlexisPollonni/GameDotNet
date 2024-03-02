58592df7f793f0cabb939b283010e2ad580ba643
AlexisPollonni <alexis.pollonni@gmail.com>
2024-03-02 19:17:59 +0100

    Add view manager event to observe main view change

--- GameDotNet.Graphics/NativeViewManager.cs
+++ GameDotNet.Graphics/NativeViewManager.cs
@@ -1,8 +1,33 @@
 using GameDotNet.Graphics.Abstractions;
+using MessagePipe;
 
 namespace GameDotNet.Graphics;
 
-public class NativeViewManager
+public sealed class NativeViewManager : IDisposable
 {
-    public INativeView? MainView { get; set; }
+    public INativeView? MainView
+    {
+        get => _mainView;
+        set
+        {
+            _mainView = value;
+            _viewChanged.Publish(value);
+        }
+    }
+
+    public ISubscriber<INativeView?> MainViewChanged { get; set; }
+    
+    
+    private INativeView? _mainView;
+    private readonly IDisposablePublisher<INativeView?> _viewChanged;
+
+    public NativeViewManager(EventFactory factory)
+    {
+        (_viewChanged, MainViewChanged) = factory.CreateEvent<INativeView?>();
+    }
+
+    public void Dispose()
+    {
+        _viewChanged.Dispose();
+    }
 }
