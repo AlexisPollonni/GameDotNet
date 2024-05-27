4aeeb3f12c512fc1d38fda2d6464c6a4589dc984
AlexisPollonni <alexis.pollonni@gmail.com>
2024-05-27 01:47:55 -0700

    [Editor] Changed editor 3D view impl to use SDL child window. Adds more supported platforms like linux

--- GameDotNet.Editor/Tools/AvaloniaNativeView.cs
+++ GameDotNet.Editor/Tools/AvaloniaNativeView.cs
@@ -7,6 +7,8 @@ using GameDotNet.Core.Tools.Extensions;
 using GameDotNet.Graphics.Abstractions;
 using MessagePipe;
 using Silk.NET.Core.Contexts;
+using Silk.NET.SDL;
+using Silk.NET.Windowing.Sdl;
 using IInputContext = GameDotNet.Input.Abstract.IInputContext;
 
 namespace GameDotNet.Editor.Tools;
@@ -31,7 +33,7 @@ internal sealed class AvaloniaNativeView : INativeView, IDisposable
 
     public AvaloniaNativeView(Control host, IPlatformHandle handle, EventFactory eventFactory)
     {
-        var win = (Window)host.GetVisualRoot()!;
+        var win = (Avalonia.Controls.Window)host.GetVisualRoot()!;
         _renderScaling = win.RenderScaling;
 
         (_resized, Resized) = eventFactory.CreateEvent<System.Drawing.Size>();
@@ -45,11 +47,25 @@ internal sealed class AvaloniaNativeView : INativeView, IDisposable
         host.GotFocus += (_, _) => _focusChanged.Publish(true);
         host.LostFocus += (_, _) => _focusChanged.Publish(false);
 
-        Native = new AvaloniaNativeWindow(handle);
+        Native = CreateFromAvaloniaControl(host, handle);
         Input = new AvaloniaInputContext(host, eventFactory);
 
         //If control has already been sized set size and send event
         if (host.Bounds != default) Resize(host.Bounds.Size);
+    }
+
+    private INativeWindow CreateFromAvaloniaControl(Control host, IPlatformHandle parent)
+    {
+        unsafe
+        {
+            var sdlView = SdlWindowing.CreateFrom((void*)parent.Handle);
+
+            var handle = SdlWindowing.GetHandle(sdlView);
+
+            var api = SdlWindowing.GetExistingApi(sdlView);
+
+            return new SdlNativeWindow(api, handle);
+        }
     }
 
     private System.Drawing.Size AvaloniaPixelSizeToSize(Size size)
@@ -64,32 +80,6 @@ internal sealed class AvaloniaNativeView : INativeView, IDisposable
         var newSize = AvaloniaPixelSizeToSize(size);
         Size = newSize;
         _resized.Publish(newSize);
-    }
-
-    private class AvaloniaNativeWindow : INativeWindow
-    {
-        public AvaloniaNativeWindow(IPlatformHandle handle)
-        {
-            if (OperatingSystem.IsWindows())
-            {
-                Kind = NativeWindowFlags.Win32;
-                Win32 = new ValueTuple<nint, nint, nint>(handle.Handle, IntPtr.Zero, WinApi.GetModuleHandle(null));
-            }
-        }
-
-        public NativeWindowFlags Kind { get; }
-        public (nint Display, nuint Window)? X11 { get; }
-        public nint? Cocoa { get; }
-        public (nint Display, nint Surface)? Wayland { get; }
-        public nint? WinRT { get; }
-        public (nint Window, uint Framebuffer, uint Colorbuffer, uint ResolveFramebuffer)? UIKit { get; }
-        public (nint Hwnd, nint HDC, nint HInstance)? Win32 { get; }
-        public (nint Display, nint Window)? Vivante { get; }
-        public (nint Window, nint Surface)? Android { get; }
-        public nint? Glfw { get; }
-        public nint? Sdl { get; }
-        public nint? DXHandle { get; }
-        public (nint? Display, nint? Surface)? EGL { get; }
     }
 
     public void Dispose()
