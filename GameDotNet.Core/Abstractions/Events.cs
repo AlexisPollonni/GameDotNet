d4b99600e9df1c787e75642e6df444526381b0ce
AlexisPollonni <43394202+AlexisPollonni@users.noreply.github.com>
2025-10-25 03:27:25 +0200

    feat: Add entity event publisher for ECS component updates

--- GameDotNet.Core/Abstractions/Events.cs
+++ GameDotNet.Core/Abstractions/Events.cs
@@ -1,4 +1,13 @@
+using Arch.Core;
+
 namespace GameDotNet.Core.Abstractions;
 
 public record struct EngineStartedEvent;
 public record struct EngineStoppingEvent;
+
+
+public record struct EntityCreatedEvent(Entity New);
+public record struct EntityDestroyedEvent(Entity Destroyed);
+public record struct EntityComponentAddedEvent(Entity Entity, ComponentType Type);
+public record struct EntityComponentSetEvent(Entity Entity, ComponentType Type);
+public record struct EntityComponentRemovedEvent(Entity Entity, ComponentType Type);
---
+++ GameDotNet.Core/Services/EntityUpdatePublisher.cs
@@ -0,0 +1,65 @@
+using Arch.Core;
+using GameDotNet.Core.Abstractions;
+using MessagePipe;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GameDotNet.Core.Services;
+
+public static class ServiceCollectionExtensions
+{
+    private class EntityUpdatePublisher
+    {
+        public EntityUpdatePublisher(SceneManager scene,
+                                     IPublisher<EntityCreatedEvent> entityCreatedPublisher,
+                                     IPublisher<EntityDestroyedEvent> entityDestroyedPublisher)
+        {
+            scene.World.SubscribeEntityCreated((in entity) => { entityCreatedPublisher.Publish(new(entity)); });
+
+            scene.World.SubscribeEntityDestroyed((in entity) => { entityDestroyedPublisher.Publish(new(entity)); });
+        }
+    }
+
+    private class ComponentPublisher<TComponent>
+    {
+        public ComponentPublisher(SceneManager sceneManager,
+                                  IPublisher<EntityComponentAddedEvent> componentAddedPublisher,
+                                  IPublisher<EntityComponentSetEvent> componentSetPublisher,
+                                  IPublisher<EntityComponentRemovedEvent> componentRemovedPublisher)
+        {
+            sceneManager.World.SubscribeComponentAdded<TComponent>((in entity, ref comp) =>
+            {
+                componentAddedPublisher.Publish(
+                    new(entity, typeof(TComponent)));
+            });
+
+            sceneManager.World.SubscribeComponentSet<TComponent>((in entity, ref comp) =>
+            {
+                componentSetPublisher.Publish(
+                    new(entity, typeof(TComponent)));
+            });
+
+            sceneManager.World.SubscribeComponentRemoved<TComponent>((in entity, ref comp) =>
+            {
+                componentRemovedPublisher.Publish(
+                    new(entity, typeof(TComponent)));
+            });
+        }
+    }
+
+    /// <summary>
+    /// Registers an ECS component type if not already registered in the singleton registry.
+    /// </summary>
+    /// <typeparam name="T">Ecs component type to register</typeparam>
+    /// <returns></returns>
+    public static IServiceCollection AddEcsComponent<T>(this IServiceCollection services)
+    {
+        if (ComponentRegistry.Has<T>()) return services;
+
+        ComponentRegistry.Add<T>();
+
+        services.TryAddActivatedSingleton<EntityUpdatePublisher>();
+        services.TryAddActivatedSingleton<ComponentPublisher<T>>();
+
+        return services;
+    }
+}
