02b335573a312f53d059b683e1cda3e700140a06
AlexisPollonni <43394202+AlexisPollonni@users.noreply.github.com>
2025-11-01 19:00:36 +0100

    refactor: add job and dependencies source generated automatic DI registrations

--- GameDotNet.Hosting/ServiceCollectionExtensions.cs
+++ GameDotNet.Hosting/ServiceCollectionExtensions.cs
@@ -1,21 +1,25 @@
+using GameDotNet.Core;
+using GameDotNet.Core.Abstractions;
+using GameDotNet.Core.Services;
+using GameDotNet.Core.Tooling;
 using GameDotNet.Graphics;
 using GameDotNet.Graphics.Assets.Assimp;
 using GameDotNet.Graphics.WGPU;
-using GameDotNet.Management;
-using GameDotNet.Management.ECS;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OpenTelemetry.Metrics;
-using Schedulers;
 using Serilog;
 using Serilog.Events;
+using ServiceScan.SourceGenerator;
 
 namespace GameDotNet.Hosting;
 
 public static class ServiceCollectionExtensions
 {
-    public static IHostApplicationBuilder AddEngineFileLogger(this IHostApplicationBuilder builder, LogEventLevel level = LogEventLevel.Verbose)
+    public static IHostApplicationBuilder AddEngineFileLogger(this IHostApplicationBuilder builder,
+                                                              LogEventLevel level = LogEventLevel.Verbose)
     {
         var configuration = Engine.CreateFileLoggerConfig(builder.Environment.ApplicationName, level);
         builder.Logging.AddSerilog(configuration.CreateLogger(), true);
@@ -23,58 +27,82 @@ public static class ServiceCollectionExtensions
         return builder;
     }
 
-    /// <summary>
-    /// Registers all necessary services to run the core
-    /// </summary>
     /// <param name="services"></param>
-    /// <returns></returns>
-    public static IServiceCollection AddCoreSystemServices(this IServiceCollection services)
+    extension(IServiceCollection services)
     {
-        services.AddMetrics()
-                .AddMessagePipe().Services
-                .AddSingleton<JobScheduler>(_ => new(new()
-                {
-                    MaxExpectedConcurrentJobs = 100,
-                    ThreadPrefixName = "UpdateJob"
-                }))
-                .AddSingleton<Universe>()
-                .AddSingleton<SceneManager>()
-                .AddTransient<AssimpNetImporter>()
-                .AddSingleton<SlangContext>(p => new(p.GetRequiredService<ILogger<SlangContext>>(), ["Assets/"]))
-                .AddSingleton<NativeViewManager>()
-                .AddSingleton<WebGpuRenderer>()
-                .AddSystem<WebGpuRenderSystem>()
-                .AddSystem<CameraSystem>();
-        
-        return services;
-    }
-
-    public static IServiceCollection AddEngineHostedService(this IServiceCollection services) =>
-        services.AddHostedService<EngineStartupHostedService>();
-
-    public static IServiceCollection AddEngineInstrumentation(this IServiceCollection services)
-    {
-        const string universeMeterName ="Universe.Updates";
-        var boundaries = Enumerable.Range(0, 10000).Select(i => i / 100D).ToArray();
-        
-        services.ConfigureOpenTelemetryMeterProvider(builder =>
+        /// <summary>
+        /// Registers all necessary services to run the core
+        /// </summary>
+        /// <returns></returns>
+        public IServiceCollection AddCoreSystemServices()
         {
-            builder.AddMeter(universeMeterName)
-                //Change to exponential histogram view when https://github.com/dotnet/aspire/issues/4381 is fixed
-                .AddView(instrument => instrument.Meter.Name == universeMeterName ? new ExplicitBucketHistogramConfiguration
-                {
-                    Boundaries = boundaries
-                } : null);
-        });
-        
-        
-        return services;
+            return services.AddMetrics()
+                    .AddMessagePipe().Services
+                    .AddSingleton<JobManager>()
+                    .AddSingleton<SceneManager>()
+                    .AddTransient<AssimpNetImporter>()
+                    .AddSingleton<SlangContext>(p => new(p.GetRequiredService<ILogger<SlangContext>>(), ["Assets/"]))
+                    .AddSingleton<WebGpuRenderer>()
+                    
+                    .AddPooled<PooledValueTaskSource>();
+        }
+
+        public IServiceCollection AddEngineHostedService() =>
+            services.AddHostedService<EngineStartupHostedService>();
+
+        public IServiceCollection AddEngineInstrumentation()
+        {
+            const string universeMeterName = "Universe.Updates";
+            var boundaries = Enumerable.Range(0, 10000).Select(i => i / 100D).ToArray();
+
+            services.ConfigureOpenTelemetryMeterProvider(builder =>
+            {
+                builder.AddMeter(universeMeterName)
+                       //Change to exponential histogram view when https://github.com/dotnet/aspire/issues/4381 is fixed
+                       .AddView(instrument => instrument.Meter.Name == universeMeterName
+                                    ? new ExplicitBucketHistogramConfiguration
+                                    {
+                                        Boundaries = boundaries
+                                    }
+                                    : null);
+            });
+
+
+            return services;
+        }
+    }
+}
+
+public static partial class GeneratedServiceCollectionExtensions
+{
+    [GenerateServiceRegistrations(AssignableTo = typeof(IUpdateJob), AssemblyNameFilter = "GameDotNet*", Lifetime = ServiceLifetime.Singleton)]
+    public static partial IServiceCollection AddUpdateJobs(this IServiceCollection services);
+    
+    [GenerateServiceRegistrations(AssignableTo = typeof(IJobDependsOn<>),
+                                  AssemblyNameFilter = "GameDotNet*",
+                                  CustomHandler = nameof(RegisterJobAndDependency))]
+    public static partial IServiceCollection AddJobDependencies(this IServiceCollection services);
+
+    private static void RegisterJobAndDependency<TJob, TJobDependency>(IServiceCollection services)
+        where TJob : class, IJobDependsOn<TJobDependency>, IUpdateJob where TJobDependency : IUpdateJob
+    {
+        services.TryAddSingleton<IJobDependencyGraph, DefaultJobDependencyGraph>();
+        services.AddKeyedSingleton(serviceKey: typeof(TJob),
+                                   new DefaultJobDependencyGraph.JobDependencyDescriptor(
+                                       typeof(TJob),
+                                       typeof(TJobDependency)));
+
+        services.TryAddSingleton<IUpdateJob, TJob>();
     }
 
-    public static IServiceCollection AddSystem<TSys>(this IServiceCollection services) where TSys : SystemBase
+    private class DefaultJobDependencyGraph(IServiceProvider provider) : IJobDependencyGraph
     {
-        services.AddSingleton<TSys>().AddSingleton<SystemBase, TSys>(p => p.GetRequiredService<TSys>());
+        internal record JobDependencyDescriptor(Type JobType, Type JobDependency);
 
-        return services;
+        public IEnumerable<Type> GetDependencies(Type jobType)
+        {
+            return provider.GetKeyedServices<JobDependencyDescriptor>(jobType)
+                           .Select(descriptor => descriptor.JobDependency);
+        }
     }
 }
