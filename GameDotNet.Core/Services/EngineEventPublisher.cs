a4d63d569e41548cdf3e2cb6d19ea41bc63c928f
AlexisPollonni <43394202+AlexisPollonni@users.noreply.github.com>
2025-12-27 01:10:57 +0100

    refactor: moved EngineEventPublisher to correct namespace

---
+++ GameDotNet.Core/Services/EngineEventPublisher.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+using GameDotNet.Core.Abstractions;
+
+namespace GameDotNet.Core.Services;
+
+[RegisterSingleton<EngineEventPublisher>]
+public sealed class EngineEventPublisher(IEventBus eventBus)
+{
+    public void SendKeyDown(IViewPort view, Key key) =>
+        eventBus.Publish(view, new KeyDownEvent(key));
+
+    public void SendKeyUp(IViewPort view, Key key) =>
+        eventBus.Publish(view, new KeyUpEvent(key));
+
+    public void SendMouseClickDown(IViewPort view, MouseButton button) =>
+        eventBus.Publish(view, new MouseClickDownEvent(button));
+
+    public void SendMouseClickUp(IViewPort view, MouseButton button) =>
+        eventBus.Publish(view, new MouseClickUpEvent(button));
+
+    public void SendMouseScroll(IViewPort view, Vector2 delta) =>
+        eventBus.Publish(view, new MouseScrollEvent(delta));
+
+    public void SendMouseMove(IViewPort view, Vector2 delta) =>
+        eventBus.Publish(view, new MouseMoveEvent(delta));
+    
+    public void SendCreated(IViewPort view) =>
+        eventBus.Publish(new ViewportCreatedEvent(view));
+
+    public void SendDestroyed(IViewPort view) =>
+        eventBus.Publish(new ViewportDestroyedEvent(view));
+
+    public void SendActiveChanged(IViewPort viewport)
+    {
+        eventBus.Publish(new ViewportActiveChangedEvent(_activeViewport, viewport));
+        _activeViewport = viewport;
+    }
+    
+    private IViewPort? _activeViewport;
+}
