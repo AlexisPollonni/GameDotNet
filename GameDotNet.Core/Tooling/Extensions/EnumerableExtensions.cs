af0272cef12eb07353a9ce1c2803b1ee58ff376f
AlexisPollonni <43394202+AlexisPollonni@users.noreply.github.com>
2026-03-14 02:26:14 +0100

    chore: remove unecessary helper, part of standard lib now

--- GameDotNet.Core/Tooling/Extensions/EnumerableExtensions.cs
+++ GameDotNet.Core/Tooling/Extensions/EnumerableExtensions.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using Collections.Pooled;
 
 namespace GameDotNet.Core.Tooling.Extensions;
@@ -7,10 +6,6 @@ public static class EnumerableExtensions
 {
     public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) =>
         source.Where(arg => arg is not null)!;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static IEnumerable<(T Item, int Index)> WithIndex<T>(this IEnumerable<T> source) =>
-        source.Select((item, i) => (item, i));
 
     public static void Resize<T>(this List<T> list, int size, T defaultVal)
     {
@@ -30,19 +25,22 @@ public static class EnumerableExtensions
 
     public static void Resize<T>(this List<T> list, int size) => list.Resize(size, default!);
 
-    public static unsafe ulong SizeOf<T>(this IReadOnlyList<T> list) where T : unmanaged
+    public static unsafe ulong SizeOf<T>(this IReadOnlyList<T> list)
+        where T : unmanaged
     {
         return (ulong)(sizeof(T) * list.Count);
     }
 
     public static void EnqueueRange<T>(this PooledQueue<T> queue, IEnumerable<T> range)
     {
-        foreach (var x in range) queue.Enqueue(x);
+        foreach (var x in range)
+            queue.Enqueue(x);
     }
-    
+
     public static IEnumerable<T> FlattenLevelOrder<T>(
         this IEnumerable<T> items,
-        Func<T, IEnumerable<T>?> getChildren)
+        Func<T, IEnumerable<T>?> getChildren
+    )
     {
         using var stack = new PooledQueue<T>();
         foreach (var item in items)
@@ -54,7 +52,8 @@ public static class EnumerableExtensions
             yield return current;
 
             var children = getChildren(current);
-            if (children is null) continue;
+            if (children is null)
+                continue;
 
             foreach (var child in children)
                 stack.Enqueue(child);
@@ -79,7 +78,7 @@ public static class EnumerableExtensions
     //
     //         if (root.)
     //         {
-    //             
+    //
     //         }
     //     }
     //
