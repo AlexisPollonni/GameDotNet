47f2a00bf9ea5c762d53dab5aba260f88e04c117
Alexis.Pollonni <43394202+alexispollonni@users.noreply.github.com>
2026-04-17 04:55:04 +0100

    chore: add performance improvement note in IVulkanWrapper comments

--- GameDotNet.Graphics.Vulkan/Abstractions/IVulkanWrapper.cs
+++ GameDotNet.Graphics.Vulkan/Abstractions/IVulkanWrapper.cs
@@ -2,6 +2,7 @@ using GameDotNet.Graphics.Vulkan.Wrappers;
 
 namespace GameDotNet.Graphics.Vulkan.Abstractions;
 
+//TODO: performance improvements: consider turning wrappers into generic structs, move wrapper logic to extension methods of IVulkanWrapper<TUnderlying> and add global aliases for each wrapper such as VulkanWrapper<Device> = VulkanDevice
 public interface IVulkanWrapper<out TUnderlying>
     where TUnderlying : unmanaged
 {
